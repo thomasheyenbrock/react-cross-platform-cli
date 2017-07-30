2a20bfcc968da5eede8f993f738514aac5bc6a08
Thomas Heyenbrock <thomas.heyenbrock@gmail.com>
2017-07-31 01:32:45 +0200

    change index.desktop.js for proper building

--- files/index.desktop.js
+++ files/index.desktop.js
@@ -1,22 +1,195 @@
-const electron = require('electron');
-// Module to control application life.
-const app = electron.app;
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow;
-
+const {app, BrowserWindow, dialog, shell, Menu} = require('electron');
+const fs = require('fs');
+const Module = require('module');
 const path = require('path');
 const url = require('url');
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 
-/**
- *
- */
-function createWindow() {
+// Parse command line options.
+const argv = process.argv.slice(1);
+const option = {
+    file: null,
+    help: null,
+    version: null,
+    abi: null,
+    webdriver: null,
+    modules: []
+};
+
+for (let i = 0; i < argv.length; i++) {
+    if (argv[i] === '--version' || argv[i] === '-v') {
+        option.version = true;
+        break;
+    } else if (argv[i] === '--abi') {
+        option.abi = true;
+        break;
+    } else if (argv[i].match(/^--app=/)) {
+        option.file = argv[i].split('=')[1];
+        break;
+    } else if (argv[i] === '--help' || argv[i] === '-h') {
+        option.help = true;
+        break;
+    } else if (argv[i] === '--interactive' || argv[i] === '-i') {
+        option.interactive = true;
+    } else if (argv[i] === '--test-type=webdriver') {
+        option.webdriver = true;
+    } else if (argv[i] === '--require' || argv[i] === '-r') {
+        option.modules.push(argv[++i]);
+        continue;
+    } else if (argv[i][0] === '-') {
+        continue;
+    } else {
+        option.file = argv[i];
+        break;
+    }
+}
+
+// Create default menu.
+app.once('ready', () => {
+    if (Menu.getApplicationMenu()) {
+        return;
+    }
+
+    const template = [
+        {
+            label: 'Edit',
+            submenu: [
+                {role: 'undo'},
+                {role: 'redo'},
+                {type: 'separator'},
+                {role: 'cut'},
+                {role: 'copy'},
+                {role: 'paste'},
+                {role: 'pasteandmatchstyle'},
+                {role: 'delete'},
+                {role: 'selectall'}
+            ]
+        },
+        {
+            label: 'View',
+            submenu: [
+                {role: 'reload'},
+                {role: 'forcereload'},
+                {role: 'toggledevtools'},
+                {type: 'separator'},
+                {role: 'resetzoom'},
+                {role: 'zoomin'},
+                {role: 'zoomout'},
+                {type: 'separator'},
+                {role: 'togglefullscreen'}
+            ]
+        },
+        {
+            role: 'window',
+            submenu: [
+                {role: 'minimize'},
+                {role: 'close'}
+            ]
+        },
+        {
+            role: 'help',
+            submenu: [
+                {
+                    label: 'Learn More',
+                    click () {
+                        shell.openExternal('https://electron.atom.io');
+                    }
+                },
+                {
+                    label: 'Documentation',
+                    click () {
+                        shell.openExternal(`https://github.com/electron/electron/tree/v${process.versions.electron}/docs#readme`);
+                    }
+                },
+                {
+                    label: 'Community Discussions',
+                    click () {
+                        shell.openExternal('https://discuss.atom.io/c/electron');
+                    }
+                },
+                {
+                    label: 'Search Issues',
+                    click () {
+                        shell.openExternal('https://github.com/electron/electron/issues');
+                    }
+                }
+            ]
+        }
+    ];
+
+    if (process.platform === 'darwin') {
+        template.unshift({
+            label: 'Electron',
+            submenu: [
+                {role: 'about'},
+                {type: 'separator'},
+                {
+                    role: 'services',
+                    submenu: []
+                },
+                {type: 'separator'},
+                {role: 'hide'},
+                {role: 'hideothers'},
+                {role: 'unhide'},
+                {type: 'separator'},
+                {role: 'quit'}
+            ]
+        });
+        template[1].submenu.push(
+            {type: 'separator'},
+            {
+                label: 'Speech',
+                submenu: [
+                    {role: 'startspeaking'},
+                    {role: 'stopspeaking'}
+                ]
+            }
+        );
+        template[3].submenu = [
+            {role: 'close'},
+            {role: 'minimize'},
+            {role: 'zoom'},
+            {type: 'separator'},
+            {role: 'front'}
+        ];
+    } else {
+        template.unshift({
+            label: 'File',
+            submenu: [
+                {role: 'quit'}
+            ]
+        });
+    }
+
+    const menu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(menu);
+});
+
+// Quit when all windows are closed and no other one is listening to this.
+// On OS X it is common for applications and their menu bar
+// to stay active until the user quits explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+    if (app.listeners('window-all-closed').length === 1 && !option.interactive && process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        startApp();
+    }
+});
+
+function startApp() {
     // Create the browser window.
-    mainWindow = new BrowserWindow({width: 800, height: 600});
+    mainWindow = new BrowserWindow({
+        height: 600,
+        title: "whatanapp",
+        width: 800
+    });
 
     // and load the index.html of the app in production
     // or localhost on port 3000 in development
@@ -41,27 +214,6 @@ function createWindow() {
     });
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
-
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
-});
-
-app.on('activate', () => {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (mainWindow === null) {
-        createWindow();
-    }
-});
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+// Start the specified app if there is one specified in command line, otherwise
+// start the default app.
+app.on('ready', startApp);
