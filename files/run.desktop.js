4f35844426c114b0054808da775006cbe8304f84
Thomas Heyenbrock <thomas.heyenbrock@gmail.com>
2017-08-01 23:07:36 +0200

    replace spawn with exec in run desktop script

--- files/run.desktop.js
+++ files/run.desktop.js
@@ -1,9 +1,8 @@
 var exec = require('child_process').exec;
-var spawn = require('child_process').spawn;
 
 exec('cross-env NODE_ENV=development electron .');
 
-var test = spawn('npm', ['run', 'web']);
-test.stdout.on('data', function (data) {
+var web = exec('npm run web');
+web.stdout.on('data', function (data) {
     console.log(data.toString());
 });
